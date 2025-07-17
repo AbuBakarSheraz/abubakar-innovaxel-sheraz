d4354288f3a1f1930a7481a3a3fdde50dbbb9b36
sherazi janjua <abubakarsheraz370@gmail.com>
2025-07-17 21:07:09 +0500

    Adding CORS headers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 5000;
 const connectDB = require('./config/db')
 const urlRoutes = require('./routes/urlRoutes');
 connectDB();
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api',urlRoutes);
