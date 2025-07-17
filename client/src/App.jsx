d8ddf169102e66a3d44423d2dac6976d2d8873c2
sherazi janjua <abubakarsheraz370@gmail.com>
2025-07-17 21:30:56 +0500

    App.jsx update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import UrlShortener from './components/UrlShortener';
 
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <UrlShortener />
+    </div>
+  );
+};
+
+export default App;
